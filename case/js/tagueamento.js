a1f5faa7a2f28048458e217a1f8b727870cf145f
mmrosatab <mmrosatab@hotmail.com>
2022-01-18 16:44:28 -0300

    Feature: Adding send page views, contact and download events

--- case/js/tagueamento.js
+++ case/js/tagueamento.js
@@ -1,6 +1,29 @@
-// Preencha este arquivo com qualquer código que você necessite para realizar a
-// coleta, desde a biblioteca analytics.js, gtag.js ou o snippet do Google Tag 
-// Manager. No último caso, não é necessário implementar a tag <noscript>.
-// O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
-// para fazer a sua coleta.
-// Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+const contactAnchor = document.getElementById("contact-anchor");
+const downloadAnchor = document.getElementById("download-anchor");
+
+contactAnchor.addEventListener("click", function () {
+  ga("send", "event", "menu", "entre_em_contato", "link_externo");
+});
+
+downloadAnchor.addEventListener("click", function () {
+  ga("send", "event", "menu", "download_pdf", "download_pdf");
+});
+
+// const loremDiv = document.getElementById("lorem-div");
+// const ipsumDiv = document.getElementById("ipsum-div");
+// const dolorDiv = document.getElementById("dolor-div");
+
+// ipsumDiv.addEventListener("click", function () {
+//   console.log("erwer");
+//   ga("send", "event", "analise", "ver_mais", "Lorem");
+// });
+
+// loremDiv.addEventListener("click", function () {
+//   console.log("erwer");
+//   ga("send", "event", "analise", "ver_mais", "Ipsum”");
+// });
+
+// dolorDiv.addEventListener("click", function () {
+//   console.log("erwer");
+//   ga("send", "event", "analise", "ver_mais", "Dolor");
+// });
